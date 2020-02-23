5fb012453fe538986d4a357e9424bf9e64c5e6bf
João Paulo <joaopaulo@MacBook-Pro-de-Joao.local>
2020-02-23 09:33:34 -0300

    Ajax working

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, render_template, jsonify
 from random import sample
-from flask_wtf import FlaskForm
-from wtforms import IntegerField
 from flask_bootstrap import Bootstrap
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
 bootstrap = Bootstrap(app)
 
-
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/')
 def index():
-    form = Num_Jogos()
-    if form.validate_on_submit():
-        num = numeros_mega(form.name.data)
-        return render_template("index.html", form=form, num = num)
-    return render_template("index.html", form=form)
+    return render_template("index.html")
 
+@app.route('/gerar')
+def gerar():
+    # run random number generator function
+    num_jogos = request.args.get("num_jogos")
+    jogos = numeros_mega(int(num_jogos))
+    return jsonify({"jogos":jogos})
 
-
+# Random number generator function
 def numeros_mega(num_jogos):
     '''
     Gerador de jogos da Mega Sena
@@ -27,17 +24,14 @@ def numeros_mega(num_jogos):
     '''
 
     jogos = []
-
     for each in range(num_jogos):
         temp = list(sample(range(1,61),6))
         temp.sort()
-        jogos.append(temp)
-
+        dezenas = ""
+        for num in temp:
+            dezenas = dezenas + str(num) + " - "
+        jogos.append(dezenas[:-3])
     return jogos
-
-
-class Num_Jogos(FlaskForm):
-    name = IntegerField('Jogos')
 
 
 if __name__ == '__main__':
